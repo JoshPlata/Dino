dda998941d97f7a9c79a11fe64074825ad733c4c
JoshPlata <43298502+JoshPlata@users.noreply.github.com>
2020-10-14 14:26:37 -0500

    Update windows.ts

--- windows.ts
+++ windows.ts
@@ -1,3 +1,9 @@
+//Variables y propiedades
+let BoolSaltar:boolean=false;
+let Canvas;
+let velocidadJuego:number=5;
+let puntos:number=0;
+
 interface Sprite{
     id: number;
     altura: number;
@@ -72,6 +78,95 @@ class Elemento implements Sprite,Mov {
     }
     set gimagen(val: string){
         this.imagen=val;
-    }
+    } 
 }
 
+let dinosaurio = new Elemento();  
+let Obstaculo= new Elemento();  
+let Obstaculo2= new Elemento();  
+let Fondo= new Elemento();  
+
+/**
+ *  Pintar el dinosaurio en el escenario, en una posicion establecida.
+ *  Se modificara solamente y.
+ */
+function dibujarPersonaje(){
+
+    /**
+     *  Crear el objeto que se pintara.
+     */          
+    dinosaurio.gid=1;
+    dinosaurio.galtura=38;
+    dinosaurio.gancho=30;
+    dinosaurio.gposX=0;
+    dinosaurio.gposY=30;
+    dinosaurio.gnombre="Dino";
+    dinosaurio.gimagen="";        
+
+}
+
+   /**
+ * @author Dan
+ * Funcion que inicia el Canvas
+ */
+
+function Inicio():void{
+        
+    AgregarEventoTeclado();
+    Canvas = document.getElementById("juego");
+    /*window.setInterval(function(){
+        FrameLoop()},1000/50);
+}*/
+
+}
+
+/**
+* @author Dan
+* Funcion que escucha el teclado
+*/
+function AgregarEventoTeclado():void{
+    window.onkeydown = function(evt){
+        switch(evt.keyCode){
+            case 32:
+                if(!BoolSaltar){
+                    //movSalto-=velocidadJuego;
+                    BoolSaltar=true;
+                }
+            break;
+           
+        }
+    }
+    return;
+}
+
+/**
+* @author Dan
+* Funcion que sube la velocidad de juego dependiendo los puntos
+*/
+function SubirVelocidadJuego():void{
+    if(puntos==300) velocidadJuego=7;
+    if(puntos==600) velocidadJuego=8;
+    if(puntos==900) velocidadJuego=9;
+    if(puntos==1200) velocidadJuego=10;
+    if(puntos==1500) velocidadJuego=11;
+    if(puntos==1800) velocidadJuego=12;
+    if(puntos==2100) velocidadJuego=13;
+    if(puntos==2400) velocidadJuego=14;
+    if(puntos==2700) velocidadJuego=15;
+    if(puntos==3000) velocidadJuego=16;
+
+    return;
+}
+
+
+
+
+
+let dinoLoop(): function():void{    
+    revisarColisiones();
+    obtenerPuntuacion();        
+    dibujarFondo();
+    dibujarPersonaje();
+    dibujarObstaculos();         
+    return;
+}
